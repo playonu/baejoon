13ee7f12281a0fceaf320d980d702d5af2b2c46c
_boooming <44767563+playonu@users.noreply.github.com>
2024-02-03 23:36:31 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.go
@@ -0,0 +1,14 @@
+func solution(dot []int) int {
+    x := dot[0]
+    y := dot[1]
+    
+    if x > 0 && y > 0 {
+        return 1
+    } else if x < 0 && y > 0 {
+        return 2
+    } else if x < 0 && y < 0 {
+        return 3
+    } else {
+        return 4
+    }
+}
